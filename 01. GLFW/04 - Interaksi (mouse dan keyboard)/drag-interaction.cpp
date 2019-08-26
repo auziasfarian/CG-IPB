99cf5076fa0203533a87285260cea804ed0408f9
Auzi Asfarian <43919811+auziasfarian@users.noreply.github.com>
2019-08-26 10:59:45 +0700

    Create drag-interaction.cpp

---
+++ 01. GLFW/04 - Interaksi (mouse dan keyboard)/drag-interaction.cpp
@@ -0,0 +1,149 @@
+//
+//  main.c
+//  openGL
+//
+//  Created by Auzi Asfarian on 20/08/19.
+//  Copyright © 2019 Auzi Asfarian. All rights reserved.
+//
+#define GLFW_SILENCE_DEPRECATION true
+#define TRUE 1
+#define FALSE 0
+#include <GLFW/glfw3.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+class Rectangle{
+public:
+    double x=300, y=300;
+    int red = 255, green = 255, blue = 255;
+    int size = 20;
+    int isSelected = FALSE;
+    int isDragged = FALSE;
+    
+    void display()
+    {
+        glBegin(GL_POLYGON);
+            glColor3ub(red, green, blue);
+            glVertex2f(-size + x,  size + y);
+            glVertex2f( size + x,  size + y);
+            glVertex2f( size + x, -size + y);
+            glVertex2f(-size + x, -size + y);
+        glEnd();
+    }
+    
+    void doIfPicked(double xpos, double ypos){
+        if(((xpos < size + x) && (xpos > -size + x)) &&
+           ((ypos < size + y) && (ypos > -size + y))){
+            red = 0; green = 255; blue = 0;
+            isSelected = TRUE;
+            if(isDragged == TRUE){
+                x = xpos;
+                y = ypos;
+            }
+        }
+        else{
+            red = green = blue = 255;
+            isSelected = FALSE;
+        }
+        
+
+    }
+    
+    void doIfKeyPressed(int key, int action){
+        if (key == GLFW_KEY_UP && action == GLFW_REPEAT)
+            y-=10;
+        if (key == GLFW_KEY_DOWN && action == GLFW_REPEAT)
+            y+=10;
+        if (key == GLFW_KEY_LEFT && action == GLFW_REPEAT)
+            x-=10;
+        if (key == GLFW_KEY_RIGHT && action == GLFW_REPEAT)
+            x+=10;
+    }
+    
+    void doIfMouseClicked(int button, int action, double posx, double posy){
+        if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS){
+            //x = posx; y = posy;
+            if(isDragged == true)
+                isDragged = false;
+            else
+                isDragged  = true;
+                
+        }
+        if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+            size--;
+    }
+};
+
+
+Rectangle rect;
+
+static void error_callback(int error, const char* description) {
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
+    rect.doIfKeyPressed(key, action);
+}
+
+static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos){
+    rect.doIfPicked(xpos, ypos);
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    double posx, posy;
+    glfwGetCursorPos(window, &posx, &posy);
+    rect.doIfMouseClicked(button, action, posx, posy);
+}
+
+double angle = 0;
+int increment = 1;
+
+int main(void) {
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+    //glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(600, 600, "Interaction", NULL, NULL);
+    if (!window){
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    
+    while (!glfwWindowShouldClose(window)){
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float) height;
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0, width, height , 0 , 1.f, -1.f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        
+        /* Color Changes
+        if(rect.red > 255) rect.red = 0;
+        if(rect.green > 255) rect.green = 0;
+        if(rect.blue > 255) rect.blue = 0;
+        rect.red ++;
+        rect.green += 5;
+        rect.blue += 10;
+        */
+        rect.display();
+        
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
